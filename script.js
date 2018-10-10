1e560f0a9728fa3969261d1c5a7295b0cc4fce2b
alex <alejandro.lliso.cosin@gmail.com>
2018-10-10 10:27:19 +0200

    Added a javascript file to cover the pass of parameters

---
+++ script.js
@@ -0,0 +1,8 @@
+var paramstr = window.location.search.substr(1);
+var paramarr = paramstr.split("&");
+var params = {};
+
+for (var i = 0; i < paramarr.length; i++) {
+  var tmparr = paramarr[i].split("=");
+  params[tmparr[0]] = tmparr[1];
+}
